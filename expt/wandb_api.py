a888ab6045c3813e890266e92df0e3d1138e62b9
Ben Dai <33288866+statmlben@users.noreply.github.com>
2024-05-25 15:11:34 +0800

    Update wandb_api.py

--- expt/wandb_api.py
+++ expt/wandb_api.py
@@ -5,9 +5,14 @@ api = wandb.Api()
 # Project is specified by <entity/project-name>
 runs = api.runs("bdai/ensLoss")
 
+df = {'dataset': [], 'model': [], 'trial': [], 'loss': [], 'test_acc': [], 'test_auc': []}
+df = pd.DataFrame(df)
 for run in runs:
     if 'VGG' in run.name:
-        perf_table = run.logged_artifacts()[0].get('perf')
+        dataset_tmp = run.name.split('-')[0]
+        model_tmp = run.name.split('-')[1]
+        perf_table = run.logged_artifacts()[2].get('perf_table')
         perf_df = pd.DataFrame(data=perf_table.data, columns=perf_table.columns)
-        perf_df.insert(loc=0, column='method', value=['BCE', 'EXP', 'Hinge', 'EnsLoss'])
-
+        perf_df.insert(loc=0, column='model', value=model_tmp)
+        perf_df.insert(loc=0, column='dataset', value=dataset_tmp)
+        df = pd.concat([df, perf_df], ignore_index=True)
